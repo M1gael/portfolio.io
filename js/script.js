465fa45aef22ab928433fad264735fae52535d42
M1gael <smit.migael.johannes@gmail.com>
2025-03-05 23:46:31 +0200

    fully presentable state. Propper scaling , dark mode , js works.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function lightswitch() {
+    var body = document.body;
+    body.classList.toggle("darkmode-body");
+
+    var elements = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, section , button , img");
+    elements.forEach(function(element) {
+        element.classList.toggle("darkmode-elements");
+    });
+
+    var img = document.getElementById("lightswitchimg");
+    if (body.classList.contains("darkmode-body")) {
+        img.src = "../img/epic.png";
+    } else {
+        img.src = "../img/boring.png";
+    }
+}
--- script.js
+++
@@ -1,9 +0,0 @@
-function lightswitch() {
-    var body = document.body;
-    body.classList.toggle("darkmode-body");
-
-    var elements = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, section");
-    elements.forEach(function(element) {
-        element.classList.toggle("darkmode-elements");
-    });
-}
